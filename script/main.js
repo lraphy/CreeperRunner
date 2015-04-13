097099112f49a31826509e160371a6e36e9cef30
Exotit <valerian.fauqueur@hetic.net>
2015-04-13 11:54:20 +0200

    effacer sur canvas

--- script/main.js
+++ script/main.js
@@ -3,3 +3,9 @@ var context = canvas.getContext("2d");
 context.fillStyle = "green";
 var x = 90; var y = 100;
 context.fillRect(x,y,50,50);
+
+context.fillStyle = "white";
+context.fillRect(0,0,canvas.width,canvas.height);
+x = 20; y = 60;
+context.fillStyle = "red";
+context.fillRect(x,y,50,80);
